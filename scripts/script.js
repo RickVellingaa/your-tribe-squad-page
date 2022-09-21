ee7f82523eee9b36c3508b802589a69378a6ff6c
MokhtarAkle <mokhtar.akle@hva.nl>
2022-09-21 10:27:38 +0200

    search bar implementation

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 var darkButton = document.querySelector("#dark-button");
 var bodyDark = document.querySelector("body");
 
+var searchFunction = document.querySelector("#search-bar");
+
+
 
 function changeDark() {
     if(bodyDark.classList.contains("darkmode")){
@@ -14,4 +17,28 @@ function changeDark() {
     }
 }
 
+function searchTest(){
+    var input = document.querySelector("#search-bar");
+    var filter = input.value.toUpperCase();
+    var ul = document.querySelector("#search-container");
+    var figcap = ul.getElementsByTagName("figcaption");
+    var fig = ul.getElementsByTagName("figure");
+    var a;
+    var i;
+    var txtValue;
+
+    for(i = 0; i < figcap.length; i++){
+        a = figcap[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+            figcap[i].style.display = "";
+            fig[i].style.display = "";
+        }
+        else{
+            figcap[i].style.display = "none";
+            fig[i].style.display = "none"
+        }
+    }
+}
 darkButton.addEventListener("click", changeDark);
+searchFunction.addEventListener("keyup", searchTest);
